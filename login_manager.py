be68c52c797a34ae3e8700e6422a19ea3e1c3741
Zorbn <public.zorb@gmail.com>
2022-05-20 18:27:42 -0700

    fix: corrected unauthorized redirect path

--- login_manager.py
+++ login_manager.py
@@ -1,3 +1,4 @@
+from flask import redirect, request
 from flask_login import LoginManager
 
 from models import User
@@ -10,3 +11,8 @@ login_manager.login_view = "login"
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
+
+
+@login_manager.unauthorized_handler
+def unauthorized_user():
+    return redirect("/login?next=" + request.path)
